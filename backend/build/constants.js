8be402e209288c77766db25ac613ad4e191079ce
Donatas Pocius <pocius.coding@gmail.com>
2023-02-26 01:39:20 +0200

    m

--- backend/build/constants.js
+++ backend/build/constants.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.API_URL = exports.API_SECRET = void 0;
 const envConfig_1 = require("./envConfig");
-exports.API_SECRET = btoa(`${envConfig_1.API_LOGIN_USERNAME}:${envConfig_1.API_LOGIN_PASSWORD}`);
+const buffer_1 = require("buffer");
+// export let API_SECRET = btoa(`${API_LOGIN_USERNAME}:${API_LOGIN_PASSWORD}`);
+exports.API_SECRET = buffer_1.Buffer.from(`${envConfig_1.API_LOGIN_USERNAME}:${envConfig_1.API_LOGIN_PASSWORD}`).toString("base64");
 exports.API_URL = {
     getData: (idOrName) => `https://api.roadgoat.com/api/v2/destinations/${idOrName}`,
 };
