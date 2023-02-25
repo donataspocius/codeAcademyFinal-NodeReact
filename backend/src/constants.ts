739e69592ac2192a38069c9b3444e5bbce57f942
Donatas Pocius <pocius.coding@gmail.com>
2023-02-26 01:34:11 +0200

    m

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,6 +1,10 @@
 import { API_LOGIN_PASSWORD, API_LOGIN_USERNAME } from "./envConfig";
+import { Buffer } from "buffer";
 
-export let API_SECRET = btoa(`${API_LOGIN_USERNAME}:${API_LOGIN_PASSWORD}`);
+// export let API_SECRET = btoa(`${API_LOGIN_USERNAME}:${API_LOGIN_PASSWORD}`);
+export let API_SECRET = Buffer.from(
+  `${API_LOGIN_USERNAME}:${API_LOGIN_PASSWORD}`
+).toString("base64");
 export const API_URL = {
   getData: (idOrName: string) =>
     `https://api.roadgoat.com/api/v2/destinations/${idOrName}`,
