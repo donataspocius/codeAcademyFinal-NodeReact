ef63d2ef685cf861bdc2d6e129edbeceeccc097f
Donatas Pocius <pocius.coding@gmail.com>
2023-02-26 02:07:32 +0200

    m

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import corsOptions from "./config/corsOptions";
 import dotenv from "dotenv";
 import { MONGODB_URI } from "./models/envConfig"; // importing string URI set for TS
 import { authenticateToken } from "./middlewares/auth.middlewares";
@@ -23,7 +24,7 @@ const PORT = process.env.PORT || 7000;
 
 // MIDDLEWARES
 app.use(express.json()); // use JSON
-app.use(cors());
+app.use(cors(corsOptions));
 
 // ENDPOINTS
 
---
+++ backend/src/config/allowedOrigins.ts
@@ -0,0 +1,1 @@
+export const allowedOrigins = ["https://donatas-travel-app.onrender.com"];
